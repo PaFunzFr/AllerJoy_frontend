b26334123535f1b94f0511e56b2d4147da258524
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-12-30 14:08:20 +0100

    Build app structure & routing, Set up features and wrappers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AppShellComponent } from './core/layout/app-shell/app-shell.component';
+import { HomeComponent } from './features/home/home.component';
+import { RecipesComponent } from './features/recipes/recipes.component';
+import { LoginComponent } from './features/login/login.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { SignupComponent } from './features/signup/signup.component';
+import { SettingsComponent } from './features/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: AppShellComponent,
+        children: [
+            { path: '', component: HomeComponent },
+            { path: 'recipes', component: RecipesComponent },
+            { path: 'contacts', component: ContactsComponent },
+            { path: 'settings', component: SettingsComponent}
+        ],
+    },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+];
--- src/app/core/layout/app-shell/app-shell.component.ts
+++ src/app/core/layout/app-shell/app-shell.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CenteredWrapperComponent } from '../wrappers/centered-wrapper/centered-wrapper.component';
 
 @Component({
   selector: 'app-app-shell',
   standalone: true,
-  imports: [],
+  imports: [CenteredWrapperComponent],
   templateUrl: './app-shell.component.html',
   styleUrl: './app-shell.component.scss'
 })

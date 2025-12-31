b2c1ef1f2ba00febeca8f798d1a5192c923e02a6
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-12-31 11:16:36 +0100

    Set up initial app structure

--- src/app/core/layout/app-shell/app-shell.component.ts
+++ src/app/core/layout/app-shell/app-shell.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CenteredWrapperComponent } from '../wrappers/centered-wrapper/centered-wrapper.component';
+import { LoginComponent } from '../../../features/login/login.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-app-shell',
   standalone: true,
-  imports: [CenteredWrapperComponent],
+  imports: [CenteredWrapperComponent, RouterOutlet],
   templateUrl: './app-shell.component.html',
   styleUrl: './app-shell.component.scss'
 })

4f20436d94afa905a4ffd8e744fbbf78b099a78a
funZ <153361103+PaFunzFr@users.noreply.github.com>
2026-01-06 13:41:33 +0100

    Add hero img on home component

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { GeneralBtnComponent } from '../../shared/ui/general-btn/general-btn.component';
+import { ButtonConfig } from '../../shared/models/buttonConfig.interface';
+import { FullscreenWrapperComponent } from '../../core/layout/wrappers/fullscreen-wrapper/fullscreen-wrapper.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FullscreenWrapperComponent,GeneralBtnComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  startBtn: ButtonConfig = {
+    label: "START",
+    backgroundColor: 'var(--color-primary)',
+    route: '/'
+  }
 
 }
